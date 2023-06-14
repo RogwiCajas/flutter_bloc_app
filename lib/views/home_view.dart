3c24989ec6921e5bad68065a994661c747d0b011
RogwiCajas <rogwinalex2@hotmail.com>
2023-06-14 12:19:30 -0500

    ss

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_app/blocs/bottom_navigation_bloc/bottom_navigation_bloc.dart';
-import 'package:flutter_bloc_app/blocs/bottom_navigation_bloc/bottom_navigation_state.dart';
+
 import 'package:flutter_bloc_app/screens/tabs/home_tab.dart';
 import 'package:flutter_bloc_app/screens/tabs/menu_tab.dart';
 import 'package:flutter_bloc_app/widgets/custom_bottom_navigation_bar.dart';
